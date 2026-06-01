ae55b2f5b2ed77a43d63832c7a11eb2d3af8fb33
Hardik Uniyal <hardikuniyal2006@gmail.com>
2026-06-01 18:34:23 +0530

    fixed SMTP timeout error 500

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 load_dotenv
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = False
